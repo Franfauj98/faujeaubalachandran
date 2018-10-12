937d5231a128ca5999811f561ab68b08007377d7
francois faujeau <francois.faujeau@gmail.com>
2018-10-12 11:54:21 +0200

    Arrow OK

--- src/shared/state/Arrow.cpp
+++ src/shared/state/Arrow.cpp
@@ -5,3 +5,27 @@ using namespace state;
 
 Arrow::Arrow (int level,int id, Position p, int life, int damage, int strikeRange, int movingRange, IdTextureUnits idTextureUnits) :
   Units(id, p, life, damage, strikeRange, movingRange, level, idTextureUnits) {}
+
+Arrow::Arrow (int level, Position p, int id){
+  switch(level){
+    case 1 :
+      Units(id, p, 1000, 100, 2, 1, level, ARROW1);
+      break;
+    case 2 :
+      Units(id, p, 2000, 200, 2, 1, level, ARROW2);
+      break;
+    case 3 :
+      Units(id, p, 3000, 300, 3, 1, level, ARROW3);
+      break;
+    case 4 :
+      Units(id, p, 4000, 400, 3, 1, level, ARROW4);
+      break;
+    default :
+      Units(id, p, 1000, 100, 2, 1, 1, ARROW1);
+      break;
+  }
+}
+
+Arrow::~Arrow(){
+
+}
