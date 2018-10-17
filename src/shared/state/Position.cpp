446ec1176893cff86c15fc59ec65cf60f3e6da53
francois faujeau <francois.faujeau@gmail.com>
2018-10-17 16:05:46 +0200

    errorDetection

--- src/shared/state/Position.cpp
+++ src/shared/state/Position.cpp
@@ -6,18 +6,22 @@ using namespace state;
 Position::Position () : x(0), y(0) {}
 
 Position::Position (int x, int y) {
-  if(x>=0 && x<10000){
-    this->x = x;
-  } else {
-    std::cerr << "X must be positive or be on the map"<< std::endl;
-    this->x = 0;
-  }
-  if(y>=0 && y<10000){
-    this->y = y;
-  } else{
-    std::cerr << "Y must be positive or be on the map"<< std::endl;
-    this->y = 0;
-  }
+  try{
+    if(x>=0 && x<10000){
+      this->x = x;
+    } else {
+      throw std::string("X must be positive or be on the map");
+      this->x = 0;
+    }
+    if(y>=0 && y<10000){
+      this->y = y;
+    } else{
+      throw std::string("Y must be positive or be on the map")  ;
+      this->y = 0;
+    }
+  } catch (std::string error){
+    std::cerr << error << std::endl;
+    }
 }
 
 int Position::getX() const{
@@ -29,19 +33,23 @@ int Position::getY() const{
 }
 
 void Position::setX(const int x) {
-  if(x>=0 && x<10000){
-    this->x = x;
-  } else {
-    std::cerr << "X must be positive or be on the map"<< std::endl;
-    this->x = 0;
-  }
+  try{
+    if(x>=0 && x<10000){
+      this->x = x;
+    } else {
+      throw std::string("X must be positive or be on the map");
+      this->x = 0;
+    }
+  } catch (std::string error){
+    std::cerr << error << std::endl;
+    }
 }
 
 void Position::setY(const int y) {
   if(y>=0 && y<10000){
     this->y = y;
   } else{
-    std::cerr << "Y must be positive or be on the map"<< std::endl;
+    throw std::string("Y must be positive or be on the map");
     this->y = 0;
   }
 }
