64d494812c30850f2a71cb5fd74087300962d448
francois faujeau <francois.faujeau@gmail.com>
2018-10-17 17:57:51 +0200

    map

--- src/shared/state/Map.cpp
+++ src/shared/state/Map.cpp
@@ -1,13 +1,30 @@
 #include "Map.h"
+#include <iostream>
 using namespace state;
+using namespace std;
 
 Map::Map(){}
 Map::~Map(){}
 
-// void Map::setMap (std::map<std::unique_ptr<Position>,std::unique_ptr<Element>> playingMap){
-//   this->playingMap = playingMap;
-// }
-//
-// std::map<std::unique_ptr<Position>,std::unique_ptr<Element>> Map::getMap (){
-//   return this->playingMap;
-// }
+void Map::addElement (std::unique_ptr<Element> elt){
+   this->playingMap.push_back(std::move(elt));
+}
+
+std::vector<std::unique_ptr<Element>> const& Map::getMap() const{
+  return this->playingMap;
+}
+
+void Map::deleteElement (int pos){
+  this->playingMap[pos] = nullptr;
+}
+
+int Map::getPositionElement (std::unique_ptr<Element> elt){
+  int pos = -1;
+  for(size_t i = 0; i<this->playingMap.size(); i++){
+    if(elt==this->playingMap[i]){
+      pos = i;
+      break;
+    }
+  }
+  return pos;
+}
