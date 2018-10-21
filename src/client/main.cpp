34cacdda26e5a0620863dac61e09b69b7256c721
francois faujeau <francois.faujeau@gmail.com>
2018-10-21 12:33:06 +0200

    renderingDiaFirtsDraft

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -2,7 +2,6 @@
 #include "string.h"
 // Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
-#include "../shared/state/Position.h"
 void testSFML() {
     sf::Texture texture;
 }
