5cb2cdd845c3d8b9cda0ecc6f7f599e587dc158f
balachandran <chirojean@hotmail.fr>
2018-10-24 15:38:48 +0200

    layer

--- src/client/render/Layer.cpp
+++ src/client/render/Layer.cpp
@@ -8,73 +8,43 @@ using namespace std;
 
 
   Layer::Layer (){
-//       Map principalMap;
-//
-//       MapCreator basicMap;
-//       principalMap.getBasicMap();
-//       const int tiles[] ={2,2,2,2,2,2,2,2,2,2,
-//                           2,2,2,2,2,2,2,2,2,2,
-//                           2,2,2,2,2,2,2,2,2,2,
-//                           2,2,2,2,1,1,1,2,2,2,
-//                           2,2,2,2,1,1,1,2,2,2,
-//                           2,2,2,2,2,2,2,2,2,2,
-//                           2,2,2,2,2,2,2,2,2,2,
-//                           2,2,1,1,2,2,2,2,2,2,
-//                           2,2,1,1,2,2,2,2,2,2,
-//                           2,2,2,2,2,2,2,2,2,2};
-//       DecorTileSet decor;
-//       basicMap.loadTexture(decor.getImageFile());
-//       basicMap.initQuads(principalMap.getSize());
-//       for (int x=0;x<principalMap.getSize();x++){
-//         for(int y=0;y<principalMap.getSize();y++){
-//           basicMap.setSpriteLocation (principalMap.getSize(),x,y,0);
-//           basicMap.setSpriteTexture(principalMap.getSize(),tiles,0,x,y);
-//         }
-//       }
-//
-//
-//       MapCreator decorMap;
-//       principalMap.getDecorMap();
-//       const int tiles[] ={2,2,2,2,2,2,2,2,2,2,
-//                           2,2,2,2,2,2,2,2,2,2,
-//                           2,2,2,2,2,2,2,2,2,2,
-//                           2,2,2,2,1,1,1,2,2,2,
-//                           2,2,2,2,1,1,1,2,2,2,
-//                           2,2,2,2,2,2,2,2,2,2,
-//                           2,2,2,2,2,2,2,2,2,2,
-//                           2,2,1,1,2,2,2,2,2,2,
-//                           2,2,1,1,2,2,2,2,2,2,
-//                           2,2,2,2,2,2,2,2,2,2};
-//
-//       decorMap.loadTexture(decor.getImageFile());
-//       decorMap.initQuads(principalMap.getSize());
-//       for (int x=0;x<principalMap.getSize();x++){
-//         for(int y=0;y<principalMap.getSize();y++){
-//           decorMap.setSpriteLocation (principalMap.getSize(),x,y,0);
-//           decorMap.setSpriteTexture(principalMap.getSize(),tiles,0,x,y);
-//         }
-//       }
-//
-//       MapCreator buildingMap;
-//       principalMap.getBuildingsMap();
-//       const int tiles[] ={2,2,2,2,2,2,2,2,2,2,
-//                           2,2,2,2,2,2,2,2,2,2,
-//                           2,2,2,2,2,2,2,2,2,2,
-//                           2,2,2,2,1,1,1,2,2,2,
-//                           2,2,2,2,1,1,1,2,2,2,
-//                           2,2,2,2,2,2,2,2,2,2,
-//                           2,2,2,2,2,2,2,2,2,2,
-//                           2,2,1,1,2,2,2,2,2,2,
-//                           2,2,1,1,2,2,2,2,2,2,
-//                           2,2,2,2,2,2,2,2,2,2};
-//       BuildingTileSet building;
-//       buildingMap.loadTexture(building.getImageFile());
-//       buildingMap.initQuads(principalMap.getSize());
-//       for (int x=0;x<principalMap.getSize();x++){
-//         for(int y=0;y<principalMap.getSize();y++){
-//           buildingMap.setSpriteLocation (principalMap.getSize(),x,y,0);
-//           buildingMap.setSpriteTexture(principalMap.getSize(),tiles,0,x,y);
-//         }
+      // Map principalMap;
+      //
+      // MapCreator basicMap;
+      // const int basicId = principalMap.getBasicMapId();
+      // DecorTileSet decor;
+      // basicMap.loadTexture(decor.getImageFile());
+      // basicMap.initQuads(principalMap.getSize());
+      // for (int x=0;x<principalMap.getSize();x++){
+      //   for(int y=0;y<principalMap.getSize();y++){
+      //     basicMap.setSpriteLocation (principalMap.getSize(),x,y,0);
+      //     basicMap.setSpriteTexture(principalMap.getSize(),basicId,0,x,y);
+      //   }
+      // }
+
+      //
+      // MapCreator decorMap;
+      // const int decorId = principalMap.getDecorMapId();
+      //
+      // decorMap.loadTexture(decor.getImageFile());
+      // decorMap.initQuads(principalMap.getSize());
+      // for (int x=0;x<principalMap.getSize();x++){
+      //   for(int y=0;y<principalMap.getSize();y++){
+      //     decorMap.setSpriteLocation (principalMap.getSize(),x,y,0);
+      //     decorMap.setSpriteTexture(principalMap.getSize(),decorId,0,x,y);
+      //   }
+      // }
+      //
+      // MapCreator buildingMap;
+      // cons int buildingsId = principalMap.getBuildingsMapId();
+      // BuildingTileSet building;
+      // buildingMap.loadTexture(building.getImageFile());
+      // buildingMap.initQuads(principalMap.getSize());
+      // for (int x=0;x<principalMap.getSize();x++){
+      //   for(int y=0;y<principalMap.getSize();y++){
+      //     buildingMap.setSpriteLocation (principalMap.getSize(),x,y,0);
+      //     buildingMap.setSpriteTexture(principalMap.getSize(),buildingsId,0,x,y);
+      //   }
       }
   Layer::~Layer (){
 
