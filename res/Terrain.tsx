6753fa1d836e211b0c710f534d6bebd418ff3962
francois faujeau <francois.faujeau@gmail.com>
2018-10-24 08:28:10 +0200

    map

--- res/Terrain.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="Terrain" tilewidth="64" tileheight="32" spacing="1" margin="1" tilecount="294" columns="14">
- <image source="../Sprites/Patron/Terrain.png" width="960" height="720"/>
-</tileset>
