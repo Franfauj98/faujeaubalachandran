78a0c1bc4095b7dcb7a933a54249ddb0a177e337
balachandran <chirojean@hotmail.fr>
2018-11-02 14:44:25 +0100

    click

---
+++ src/shared/engine/Click.cpp
@@ -0,0 +1,24 @@
+#include "Click.h"
+#include <iostream>
+//using namespace state;
+//using namespace render;
+using namespace engine;
+
+// Click::Click (sf::RenderWindow& window){
+// //   sf::Event event;
+// //
+// // // tant qu'il y a des évènements à traiter...
+// // while (window.pollEvent(event))
+// // {
+// //     // on regarde le type de l'évènement...
+// //     if (event.type == sf::Event::MouseButtonPressed)
+// //     {
+// //         if (event.mouseButton.button == sf::Mouse::Right)
+// //         {
+// //             std::cout << "the right button was pressed" << std::endl;
+// //             std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+// //             std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+// //         }
+// //     }
+// // }
+// }
