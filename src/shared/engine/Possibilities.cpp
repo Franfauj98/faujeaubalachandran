d0e0d21041cb5a31064cdc10181501e8d7b0dac7
francois faujeau <francois.faujeau@gmail.com>
2018-11-07 13:24:46 +0100

    test

--- src/shared/engine/Possibilities.cpp
+++ src/shared/engine/Possibilities.cpp
@@ -22,50 +22,55 @@ void Possibilities::execute (state::Observable& map,int x, int y, int element){
     }
     Position pos(x,y);
     selectedMap[x+25*y]=move(unique_ptr<Element> (new Decor(SELECTED,pos)));
+    std::vector<std::vector<int>> mapMatrix = map.getAllMaps().getMapMatrix();
     if (element==10 || element==14 || element==18 || element==22){
       if (x+1>=0 && x+1<25){
-        if(map.getAllMaps().getMapMatrix()[x+1][y]==2 || map.getAllMaps().getMapMatrix()[x+1][y]==9 ||
-        map.getAllMaps().getMapMatrix()[x+1][y]==26 || map.getAllMaps().getMapMatrix()[x+1][y]==27 ||
-        map.getAllMaps().getMapMatrix()[x+1][y]==28 || map.getAllMaps().getMapMatrix()[x+1][y]==29 ||
-        map.getAllMaps().getMapMatrix()[x+1][y]==10 || map.getAllMaps().getMapMatrix()[x+1][y]==14 ||
-        map.getAllMaps().getMapMatrix()[x+1][y]==16 || map.getAllMaps().getMapMatrix()[x+1][y]==22){
+        if(mapMatrix[x+1][y]==2 || mapMatrix[x+1][y]==9 ||
+          mapMatrix[x+1][y]==26 || mapMatrix[x+1][y]==27 ||
+          mapMatrix[x+1][y]==28 || mapMatrix[x+1][y]==29 ||
+          mapMatrix[x+1][y]==10 || mapMatrix[x+1][y]==14 ||
+          mapMatrix[x+1][y]==16 || mapMatrix[x+1][y]==22){
           Position pos2(x+1,y);
           selectedMap[x+1+25*y]=move(unique_ptr<Element> (new Decor(SELECTED,pos2)));
           cout<<endl;
           cout<<map.getAllMaps().getMapMatrix()[x+1][y]<<endl;
+          cout<<"coucou1"<<endl;
         }
       }
       if (x-1>=0 && x-1<25){
-        if(map.getAllMaps().getMapMatrix()[x-1][y]==2 || map.getAllMaps().getMapMatrix()[x-1][y]==9 ||
-        map.getAllMaps().getMapMatrix()[x-1][y]==26 || map.getAllMaps().getMapMatrix()[x-1][y]==27 ||
-        map.getAllMaps().getMapMatrix()[x-1][y]==28 || map.getAllMaps().getMapMatrix()[x-1][y]==29 ||
-        map.getAllMaps().getMapMatrix()[x-1][y]==10 || map.getAllMaps().getMapMatrix()[x-1][y]==14 ||
-        map.getAllMaps().getMapMatrix()[x-1][y]==16 || map.getAllMaps().getMapMatrix()[x-1][y]==22){
+        if(mapMatrix[x-1][y]==2 || mapMatrix[x-1][y]==9 ||
+          mapMatrix[x-1][y]==26 || mapMatrix[x-1][y]==27 ||
+          mapMatrix[x-1][y]==28 || mapMatrix[x-1][y]==29 ||
+          mapMatrix[x-1][y]==10 || mapMatrix[x-1][y]==14 ||
+          mapMatrix[x-1][y]==16 || mapMatrix[x-1][y]==22){
           Position pos3(x-1,y);
           selectedMap[x-1+25*y]=move(unique_ptr<Element> (new Decor(SELECTED,pos3)));
           cout<<map.getAllMaps().getMapMatrix()[x-1][y]<<endl;
+          cout<<"coucou2"<<endl;
         }
       }
       if (y+1>=0 && y+1<25){
-        if(map.getAllMaps().getMapMatrix()[x][y+1]==2 || map.getAllMaps().getMapMatrix()[x][y+1]==9 ||
-        map.getAllMaps().getMapMatrix()[x][y+1]==26 || map.getAllMaps().getMapMatrix()[x][y+1]==27 ||
-        map.getAllMaps().getMapMatrix()[x][y+1]==28 || map.getAllMaps().getMapMatrix()[x][y+1]==29||
-        map.getAllMaps().getMapMatrix()[x][y+1]==10 || map.getAllMaps().getMapMatrix()[x][y+1]==14 ||
-        map.getAllMaps().getMapMatrix()[x][y+1]==16 || map.getAllMaps().getMapMatrix()[x][y+1]==22){
+        if(mapMatrix[x][y+1]==2 || mapMatrix[x][y+1]==9 ||
+        mapMatrix[x][y+1]==26 || mapMatrix[x][y+1]==27 ||
+        mapMatrix[x][y+1]==28 || mapMatrix[x][y+1]==29||
+        mapMatrix[x][y+1]==10 || mapMatrix[x][y+1]==14 ||
+        mapMatrix[x][y+1]==16 || mapMatrix[x][y+1]==22){
           Position pos4(x,y+1);
           selectedMap[x+25*(y+1)]=move(unique_ptr<Element> (new Decor(SELECTED,pos4)));
           cout<<map.getAllMaps().getMapMatrix()[x][y+1]<<endl;
+          cout<<"coucou3"<<endl;
         }
       }
       if (y-1>=0 && y-1<25){
-        if(map.getAllMaps().getMapMatrix()[x][y-1]==2 || map.getAllMaps().getMapMatrix()[x][y-1]==9 ||
-        map.getAllMaps().getMapMatrix()[x][y-1]==26 || map.getAllMaps().getMapMatrix()[x][y-1]==27 ||
-        map.getAllMaps().getMapMatrix()[x][y-1]==28 || map.getAllMaps().getMapMatrix()[x][y-1]==29||
-        map.getAllMaps().getMapMatrix()[x][y-1]==10 || map.getAllMaps().getMapMatrix()[x][y-1]==14 ||
-        map.getAllMaps().getMapMatrix()[x][y-1]==16 || map.getAllMaps().getMapMatrix()[x][y-1]==22){
+        if(mapMatrix[x][y-1]==2 || mapMatrix[x][y-1]==9 ||
+        mapMatrix[x][y-1]==26 || mapMatrix[x][y-1]==27 ||
+        mapMatrix[x][y-1]==28 || mapMatrix[x][y-1]==29||
+        mapMatrix[x][y-1]==10 || mapMatrix[x][y-1]==14 ||
+        mapMatrix[x][y-1]==16 || mapMatrix[x][y-1]==22){
           Position pos5(x,y-1);
           selectedMap[x+25*(y-1)]=move(unique_ptr<Element> (new Decor(SELECTED,pos5)));
           cout<<map.getAllMaps().getMapMatrix()[x][y-1]<<endl;
+          cout<<"coucou4"<<endl;
           cout<<endl;
         }
       }
