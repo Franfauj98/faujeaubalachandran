d975db5f64aab94ed4bbc0658adb66a1d7c95c10
francois faujeau <francois.faujeau@gmail.com>
2019-01-01 15:13:56 +0100

    record

--- src/shared/engine/Engine.cpp
+++ src/shared/engine/Engine.cpp
@@ -20,8 +20,11 @@ using namespace render;
 //global jsonFile for engine.
 std::ofstream outputFileJson, outputFileTxt;
 std::ifstream inputFileTxt;
-
+Json::StyledStreamWriter writer;
+std::vector<std::string> vectTest;
+string commands;
 Engine::Engine (){
+  commands = "{\"commands\" : [";
 }
 
 void Engine::openFiles(){
@@ -42,19 +45,26 @@ void Engine::addCommand(std::unique_ptr<Command> cmd, int commandId){
 
 void Engine::execute(state::Observable& principalMap){
   Json::Reader reader;
-  Json::Value value, value2;
-  Json::StyledStreamWriter writer;
+  // Json::Value value, value2, value3;
 
   while(!this->commandList.empty()){
     switch(commandListId.front()){
       case 1:{
         CaseIdentifier* ci = (CaseIdentifier*) commandList.front().get();
         if(this->record){
-          value["name"]="CaseIdentifier";
-          value["id"]="1";
-          value2["x"]=ci->getX();
-          value2["y"]=ci->getY();
-          value["properties"]=value2;
+          commands+=("{\"name\": \"CaseIdentifier\",\"id\": \"1\",\"x\": \""+to_string(ci->getX())+"\",\"y\": \""+to_string(ci->getY())+"\"},\n");
+          // vectTest.push_back("name:CaseIdentifier");
+          // vectTest.push_back("id:1");
+          // vectTest.push_back("x:"+to_string(ci->getX()));
+          // vectTest.push_back("y:"+to_string(ci->getY()));
+          // vectTest.push_back("end");
+
+          // value["name"]="CaseIdentifier";
+          // value["id"]="1";
+          // value2["x"]=ci->getX();
+          // value2["y"]=ci->getY();
+          // value["properties"]=value2;
+          // value3["command"]=value;
 
           if(outputFileTxt.is_open()){
             outputFileTxt<<"1;"+to_string(ci->getX())+";"+to_string(ci->getY())+"\n";
@@ -66,12 +76,21 @@ void Engine::execute(state::Observable& principalMap){
       case 2:{
         Possibilities* ps = (Possibilities*) commandList.front().get();
         if(this->record){
-          value["name"]="Possibilities";
-          value["id"]="2";
-          value2["x"]=ps->getX();
-          value2["y"]=ps->getY();
-          value2["element"]=ps->getElement();
-          value["properties"]=value2;
+          commands+=("{\"name\": \"Possibilities\",\"id\": \"2\",\"x\": \""+to_string(ps->getX())+"\",\"y\": \""+to_string(ps->getY())+"\",\"element\": \""+to_string(ps->getElement())+"\"},\n");
+          // vectTest.push_back("name:Possibilities");
+          // vectTest.push_back("id:2");
+          // vectTest.push_back("x:"+to_string(ps->getX()));
+          // vectTest.push_back("y:"+to_string(ps->getY()));
+          // vectTest.push_back("element:"+to_string(ps->getElement()));
+          // vectTest.push_back("end");
+
+          // value["name"]="Possibilities";
+          // value["id"]="2";
+          // value2["x"]=ps->getX();
+          // value2["y"]=ps->getY();
+          // value2["element"]=ps->getElement();
+          // value["properties"]=value2;
+          // value3["command"]=value;
 
           if(outputFileTxt.is_open()){
             outputFileTxt<<"2;"+to_string(ps->getX())+";"+to_string(ps->getY()) +";"+ to_string(ps->getElement())+"\n";
@@ -83,12 +102,21 @@ void Engine::execute(state::Observable& principalMap){
       case 3:{
         PrintStats* pst = (PrintStats*) commandList.front().get();
         if(this->record){
-          value["name"]="PrintStats";
-          value["id"]="3";
-          value2["x"]=pst->getX();
-          value2["y"]=pst->getY();
-          value2["element"]=pst->getElement();
-          value["properties"]=value2;
+          commands+=("{\"name\": \"PrintStats\",\"id\": \"3\",\"x\": \""+to_string(pst->getX())+"\",\"y\": \""+to_string(pst->getY())+"\", \"element\": \""+to_string(pst->getElement())+"\"},\n");
+          // vectTest.push_back("name:PrintStats");
+          // vectTest.push_back("id:3");
+          // vectTest.push_back("x:"+to_string(pst->getX()));
+          // vectTest.push_back("y:"+to_string(pst->getY()));
+          // vectTest.push_back("element:"+to_string(pst->getElement()));
+          // vectTest.push_back("end");
+
+          // value["name"]="PrintStats";
+          // value["id"]="3";
+          // value2["x"]=pst->getX();
+          // value2["y"]=pst->getY();
+          // value2["element"]=pst->getElement();
+          // value["properties"]=value2;
+          // value3["command"]=value;
 
           if(outputFileTxt.is_open()){
             outputFileTxt<<"3;"+to_string(pst->getX())+";"+to_string(pst->getY()) +";"+ to_string(pst->getElement())+"\n";
@@ -101,13 +129,23 @@ void Engine::execute(state::Observable& principalMap){
       case 6:{
         Move* mv = (Move*) commandList.front().get();
         if(this->record){
-          value["name"]="Move";
-          value["id"]="6";
-          value2["x"]=mv->getX();
-          value2["y"]=mv->getY();
-          value2["x2"]=mv->getX2();
-          value2["y2"]=mv->getY2();
-          value["properties"]=value2;
+          commands+=("{\"name\": \"Move\",\"id\": \"6\",\"x\": \""+to_string(mv->getX())+"\",\"y\": \""+to_string(mv->getY())+"\",\"x2\": \""+to_string(mv->getX2())+"\",\"y2\": \""+to_string(mv->getY2())+"\"},\n");
+          // vectTest.push_back("name:Move");
+          // vectTest.push_back("id:6");
+          // vectTest.push_back("x:"+to_string(mv->getX()));
+          // vectTest.push_back("y:"+to_string(mv->getY()));
+          // vectTest.push_back("x2:"+to_string(mv->getX2()));
+          // vectTest.push_back("y2:"+to_string(mv->getY2()));
+          // vectTest.push_back("end");
+
+          // value["name"]="Move";
+          // value["id"]="6";
+          // value2["x"]=mv->getX();
+          // value2["y"]=mv->getY();
+          // value2["x2"]=mv->getX2();
+          // value2["y2"]=mv->getY2();
+          // value["properties"]=value2;
+          // value3["command"]=value;
 
           if(outputFileTxt.is_open()){
             outputFileTxt<<"6;"+to_string(mv->getX())+";"+to_string(mv->getY()) + ";" +to_string(mv->getX2())+";"+to_string(mv->getY2())+"\n";
@@ -119,13 +157,23 @@ void Engine::execute(state::Observable& principalMap){
       case 7:{
         Attack* at = (Attack*) commandList.front().get();
         if(this->record){
-          value["name"]="Attack";
-          value["id"]="7";
-          value2["x"]=at->getX();
-          value2["y"]=at->getY();
-          value2["x2"]=at->getX2();
-          value2["y2"]=at->getY2();
-          value["properties"]=value2;
+          commands+=("{\"name\": \"Attack\",\"id\": \"7\",\"x\": \""+to_string(at->getX())+"\",\"y\": \""+to_string(at->getY())+"\",\"x2\": \""+to_string(at->getX2())+"\",\"y2\": \""+to_string(at->getY2())+"\"},\n");
+          // vectTest.push_back("name:Attack");
+          // vectTest.push_back("id:7");
+          // vectTest.push_back("x:"+to_string(at->getX()));
+          // vectTest.push_back("y:"+to_string(at->getY()));
+          // vectTest.push_back("x2:"+to_string(at->getX2()));
+          // vectTest.push_back("y2:"+to_string(at->getY2()));
+          // vectTest.push_back("end");
+
+          // value["name"]="Attack";
+          // value["id"]="7";
+          // value2["x"]=at->getX();
+          // value2["y"]=at->getY();
+          // value2["x2"]=at->getX2();
+          // value2["y2"]=at->getY2();
+          // value["properties"]=value2;
+          // value3["command"]=value;
 
           if(outputFileTxt.is_open()){
             outputFileTxt<<"7;"+to_string(at->getX())+";"+to_string(at->getY()) + ";" +to_string(at->getX2())+";"+to_string(at->getY2())+"\n";
@@ -137,11 +185,19 @@ void Engine::execute(state::Observable& principalMap){
       case 5:{
         LevelUp* lu = (LevelUp*) commandList.front().get();
         if(this->record){
-          value["name"]="LevelUp";
-          value["id"]="5";
-          value2["x"]=lu->getX();
-          value2["y"]=lu->getY();
-          value["properties"]=value2;
+          commands+=("{\"name\": \"LevelUp\",\"id\": \"5\",\"x\": \""+to_string(lu->getX())+"\",\"y\": \""+to_string(lu->getY())+"\"},\n");
+          // vectTest.push_back("name:LevelUp");
+          // vectTest.push_back("id:5");
+          // vectTest.push_back("x:"+to_string(lu->getX()));
+          // vectTest.push_back("y:"+to_string(lu->getY()));
+          // vectTest.push_back("end");
+
+          // value["name"]="LevelUp";
+          // value["id"]="5";
+          // value2["x"]=lu->getX();
+          // value2["y"]=lu->getY();
+          // value["properties"]=value2;
+          // value3["command"]=value;
 
           if(outputFileTxt.is_open()){
             outputFileTxt<<"5;"+to_string(lu->getX())+";"+to_string(lu->getY())+"\n";
@@ -153,14 +209,26 @@ void Engine::execute(state::Observable& principalMap){
       case 4:{
         CreateUnit* cu = (CreateUnit*) commandList.front().get();
         if(this->record){
-          value["name"]="Attack";
-          value["id"]="4";
-          value2["x"]=cu->getX();
-          value2["y"]=cu->getY();
-          value2["x2"]=cu->getX2();
-          value2["y2"]=cu->getY2();
-          value2["unit"]=cu->getUnit();
-          value["properties"]=value2;
+          commands+=("{\"name\": \"CreateUnit\",\"id\": \"4\",\"x\": \""+to_string(cu->getX())+"\",\"y\": \""+to_string(cu->getY())+"\",\"x2\": \""+to_string(cu->getX2())+"\",\"y2\": \""+to_string(cu->getY2())+
+          "\",\"unit\": \"" +to_string(cu->getUnit())+"\"},\n");
+          // vectTest.push_back("name:CreateUnit");
+          // vectTest.push_back("id:4");
+          // vectTest.push_back("x:"+to_string(cu->getX()));
+          // vectTest.push_back("y:"+to_string(cu->getY()));
+          // vectTest.push_back("x2:"+to_string(cu->getX2()));
+          // vectTest.push_back("y2:"+to_string(cu->getY2()));
+          // vectTest.push_back("unit:"+to_string(cu->getUnit()));
+          // vectTest.push_back("end");
+
+          // value["name"]="CreateUnit";
+          // value["id"]="4";
+          // value2["x"]=cu->getX();
+          // value2["y"]=cu->getY();
+          // value2["x2"]=cu->getX2();
+          // value2["y2"]=cu->getY2();
+          // value2["unit"]=cu->getUnit();
+          // value["properties"]=value2;
+          // value3["command"]=value;
 
           if(outputFileTxt.is_open()){
             outputFileTxt<<"4;"+to_string(cu->getX())+";"+to_string(cu->getY()) + ";" +to_string(cu->getX2())+";"+to_string(cu->getY2()) +";"+ to_string(cu->getUnit())+"\n";
@@ -176,9 +244,9 @@ void Engine::execute(state::Observable& principalMap){
     this->commandListIdPrev.push_back(commandListId.front());
     this->commandList.pop();
     this->commandListId.pop();
-    if(this->record){
-      writer.write(outputFileJson, value);
-    }
+    // if(this->record){
+    //   writer.write(outputFileJson, value3);
+    // }
   }
 }
 
@@ -292,7 +360,14 @@ vector<string> explode2(string const & s, const char& c){
 
 void Engine::replay(state::Observable& principalMap){
   inputFileTxt.open("cmdTxt.txt");
+  Json::Reader reader;
+  Json::Value val;
+  reader.parse("command", val);
   string tmp;
+
+  Json::Value commands;
+  Json::Reader reader;
+
   if(inputFileTxt.is_open()){
     while(getline(inputFileTxt, tmp)){
       vector<string> commands = explode2(tmp, ';');
@@ -402,7 +477,52 @@ std::deque<int> Engine::getCommandListIdPrev(){
 
 Engine::~Engine (){
   if(this->record){
+    commands.pop_back();
+    commands.pop_back();
+    commands+="]\n}";
+    std::cout << commands << '\n';
+    // Json::Value value, value2, value3;
+    // Json::StyledWriter fast;
+    // string sFast;
+    // for(size_t i = 0; i < vectTest.size(); i++){
+    //   std::cout << vectTest[i] << '\n';
+    //   if(vectTest[i] == "end"){
+    //     value3["command"]=value;
+    //     // writer.write(outputFileJson, value3);
+    //   } else {
+    //     vector<string> toFill = explode2(vectTest[i], ':');
+    //     if(toFill[0]=="id"){
+    //       value["id"]=toFill[1];
+    //     }
+    //     if(toFill[0]=="x"){
+    //       value["x"]=toFill[1];
+    //     }
+    //     if(toFill[0]=="y"){
+    //       value["y"]=toFill[1];
+    //     }
+    //   }
+    //
+      // value["name"]=vectTest[i];
+      // value["id"]=vectTest[i];
+      // value2["x"]=cu->getX();
+      // value2["y"]=cu->getY();
+      // value2["x2"]=cu->getX2();
+      // value2["y2"]=cu->getY2();
+      // value2["unit"]=cu->getUnit();
+      // value["properties"]=value2;
+      // value3["command"]=value;
+    //
+    // }
+    // sFast = fast.write(value3);
+    outputFileJson<<commands;
+
     outputFileJson.close();
     outputFileTxt.close();
   }
+  // value2["unit"]=cu->getUnit();
+
+// if(this->record){
+//   writer.write(outputFileJson, value3);
+// }
+
 }
